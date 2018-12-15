2129bec64c97182119557f235d63507d535db218
pkr5025 <44106322+pkr5025@users.noreply.github.com>
2018-12-14 20:26:55 -0500

    Update ProjectEuler28.cpp

--- ProjectEuler28.cpp
+++ ProjectEuler28.cpp
@@ -1,5 +1,19 @@
 /* Project Euler Problem 28 from projecteuler.net
  by Philip Klein-Rodick
+ Problem Statement:
+ 
+ Starting with the number 1 and moving to the right in a clockwise direction a 5 by 5 spiral is formed as follows:
+ 
+ 21 22 23 24 25
+ 20  7  8  9 10
+ 19  6  1  2 11
+ 18  5  4  3 12
+ 17 16 15 14 13
+
+ It can be verified that the sum of the numbers on the diagonals is 101.
+
+ What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?
+
  Solution: 669171001
  Idea: For an n by n spiral, there are (n+1)/2-1 squares (-1 since the first square is an exception since it consists of 
  just the number 1.) For each square, we must add their corner values together. For a given square i, its corner values increase 
