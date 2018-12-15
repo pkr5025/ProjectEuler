f8a390d6d5b794392d7d4753852d5052b52b4ca2
pkr5025 <44106322+pkr5025@users.noreply.github.com>
2018-12-14 21:25:02 -0500

    Update ProjectEuler24.cpp

--- ProjectEuler24.cpp
+++ ProjectEuler24.cpp
@@ -7,7 +7,7 @@
 
  What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
  
- Idea: Brute force using the nex_permutation command
+ Idea: Brute force using the next_permutation command
  Solution: 2 7 8 3 9 1 5 4 6 0
 */
 
